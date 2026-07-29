474b9c0717ead6c0dda958063b8da729e331b117
zahidking-noori <nooriibrahim06@gmail.com>
2026-07-29 00:42:14 -0400

    fix custom domain base path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'node:path'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     react(),
     {
